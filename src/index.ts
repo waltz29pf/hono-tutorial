f997d7697680d9a4c3aa8d5a886adc7e2246723b
waltz29pf <113420721+waltz29pf@users.noreply.github.com>
2025-06-14 23:01:25 +0900

    Implement basic authentication middleware and add authorization endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import { Hono } from "hono";
+import { basicAuth } from "hono/basic-auth";
 import { prettyJSON } from "hono/pretty-json";
+import auth from "./auth/auth";
 import posts from "./blogs/blogs";
 
 const app = new Hono();
 
 app.use("*", prettyJSON());
+
+app.use(
+	"/auth/*",
+	basicAuth({
+		username: "admin",
+		password: "password",
+	}),
+);
+
 app.route("/posts", posts);
+app.route("/auth", auth);
 
 // ルート
 app.get("/", (c) => {

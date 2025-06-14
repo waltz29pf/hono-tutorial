5e797d22837783091200f0914c938df92f725ab5
waltz29pf <113420721+waltz29pf@users.noreply.github.com>
2025-06-14 22:21:03 +0900

    test commit to check lefthook

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,48 @@
-import { Hono } from 'hono'
-import { prettyJSON } from 'hono/pretty-json'
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
 
-const app = new Hono()
+const app = new Hono();
 
 // 仮のブログ記事データ
-let blogPosts = [
-  {
-    id: 1,
-    title: 'ブログ記事1',
-    content: 'これはブログ記事1の内容です。',
-  },
-  {
-    id: 2,
-    title: 'ブログ記事2',
-    content: 'これはブログ記事2の内容です。',
-  },
-  {
-    id: 3,
-    title: 'ブログ記事3',
-    content: 'これはブログ記事3の内容です。',
-  },
-]
+const blogPosts = [
+	{
+		id: 1,
+		title: "ブログ記事1",
+		content: "これはブログ記事1の内容です。",
+	},
+	{
+		id: 2,
+		title: "ブログ記事2",
+		content: "これはブログ記事2の内容です。",
+	},
+	{
+		id: 3,
+		title: "ブログ記事3",
+		content: "これはブログ記事3の内容です。",
+	},
+];
 
-app.use('*', prettyJSON())
+app.use("*", prettyJSON());
 
 // ルート
-app.get('/', (c) => {
-  return c.text('Hello honou!!')
-})
+app.get("/", (c) => {
+	return c.text("Hello honou!!");
+});
 
 // ブログ記事一覧を取得する
-app.get('/posts', (c) => {
-  return c.json({posts:blogPosts})
-})
-
+app.get("/posts", (c) => {
+	return c.json({ posts: blogPosts });
+});
 
 // 個別のブログ記事を取得する
-app.get("/posts/:id", (c)=>{
-  const id = c.req.param('id')
-  const post = blogPosts.find((p) => p.id === parseInt(id))
+app.get("/posts/:id", (c) => {
+	const id = c.req.param("id");
+	const post = blogPosts.find((p) => p.id === Number.parseInt(id));
 
-  if(post) {
-    return c.json({title:post.title})
-  }else {
-    return c.json({message: '記事が見つかりませんでした。'}, 404)
-  }
+	if (post) {
+		return c.json({ title: post.title });
+	}
+	return c.json({ message: "記事が見つかりませんでした。" }, 404);
+});
 
-})
-
-export default app
+export default app;
